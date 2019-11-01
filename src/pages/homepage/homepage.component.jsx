b0851d1a192dac96cef11b9d25b63ee1a0621446
Corie Rhodes <carhodes@deakin.edu.au>
2019-11-01 21:56:12 +1100

    broken homepage into smaller components, finished styling menu-items.

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Diretory from '../../components/directory/directory.component';
+
+import './homepage.style.scss';
+
+const HomePage = () => (
+    <div className='homepage'>
+        <Diretory/>
+    </div>
+);
+
+export default HomePage;
