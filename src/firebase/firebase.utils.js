fb4d33952f04eecb73df17fe2c6c38c318bfd00f
Corie Rhodes <carhodes@deakin.edu.au>
2019-11-05 22:29:05 +1100

    intergrated firebase into application, created sign in component and sign in with google

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,28 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyBAaxbNsf2Je4FKNFjlkToqmkbP2S4LM0Q",
+    authDomain: "crwn-clothing-db-1d5f5.firebaseapp.com",
+    databaseURL: "https://crwn-clothing-db-1d5f5.firebaseio.com",
+    projectId: "crwn-clothing-db-1d5f5",
+    storageBucket: "crwn-clothing-db-1d5f5.appspot.com",
+    messagingSenderId: "927510959593",
+    appId: "1:927510959593:web:9d97cc79c2f1c04e1ea778",
+    measurementId: "G-7SJ1FXZVDY"
+  };
+
+  firebase.initializeApp(config);
+
+  export const auth = firebase.auth();
+  export const firestore = firebase.firestore();
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  provider.setCustomParameters({prompt: 'select_account'});
+
+  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+  export default firebase;
+
