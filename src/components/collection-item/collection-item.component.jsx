fade2d568537bc4525ac5a27197ede6337c4601b
Corie Rhodes <carhodes@deakin.edu.au>
2019-11-13 13:57:16 +1100

    updated shop data with new data, updated cart utils to group items in cart.

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {addItem} from '../../redux/cart/cart.actions';
+import CustomButton from '../custom-button/custom-button.component';
 
 import './collection-item.styles.scss';
+import { dispatch } from '../../../../Library/Caches/typescript/3.6/node_modules/rxjs/internal/observable/pairs';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
+const CollectionItem = ({ item, addItem }) => {
+
+  const {imageUrl, name, price} = item;
+
+  return(
   <div className='collection-item'>
     <div
       className='image'
@@ -14,7 +22,12 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
       <span className='name'>{name}</span>
       <span className='price'>{price}</span>
     </div>
-  </div>
-);
+    <CustomButton onClick={() => addItem(item)} inverted>Add To Cart</CustomButton>
+  </div>)
+};
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+  addItem: item => dispatch(addItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
