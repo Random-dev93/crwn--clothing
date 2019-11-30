94d8c14abaf10829d1318fd3613b0ac1cc4baeb9
Corie Rhodes <carhodes@deakin.edu.au>
2019-11-30 22:11:32 +1100

    minor changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import CheckoutPage from './pages/checkout/checkout.component';
-import Header from './components/header-component/header.component';
+import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
 import {setelctCurrentUser, selectCurrentUser} from './redux/user/user.selectors';
